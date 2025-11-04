15fec837a022dfbcddcd0a1e7847558b2a1a6761
Niklas Sahlberg <niklassahlberg@hotmail.com>
2025-11-04 08:26:39 +0100

    trigger deploy

--- src/app/blogg/[slug]/page.tsx
+++ src/app/blogg/[slug]/page.tsx
@@ -10,7 +10,7 @@ import { useLanguage } from '../../contexts/LanguageContext';
 const fadeInUp = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
-  transition: { duration: 0.8 }
+  transition: { duration: 0.8 } 
 };
 
 // Mobile Statistics Slider Component
