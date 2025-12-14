2e835d6bf91642abb3e2446c9b79daaa5bd8b374
Niklas Sahlberg <niklassahlberg@hotmail.com>
2025-12-14 12:52:33 +0100

    Funkar nu

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/7.x/**',
+      },
+    ],
+    domains: ['localhost'],
+    dangerouslyAllowSVG: true,
+  }
 };
 
 export default nextConfig;
