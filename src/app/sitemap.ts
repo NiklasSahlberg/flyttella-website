09cdaa1cd0fcffa871a423fb61ccd6cca2b817be
Niklas Sahlberg <niklassahlberg@hotmail.com>
2025-12-14 12:46:01 +0100

    Trigger deploy

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -36,7 +36,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     }, 
     {
-      url: `${baseUrl}/dodsbo-stadning`,
+      url: `${baseUrl}/dodsbo-stadning`,  
       lastModified: new Date(),
       changeFrequency: 'monthly' as const,
       priority: 0.9,
