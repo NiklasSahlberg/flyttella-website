41ece92d107c61fb3d3d4a47c869212d8fb04231
Niklas Sahlberg <niklassahlberg@hotmail.com>
2026-03-14 14:34:27 +0100

    Final form

--- src/app/components/FlyttoffertForm.tsx
+++ src/app/components/FlyttoffertForm.tsx
@@ -1081,7 +1081,7 @@ export default function FlyttoffertForm({ mode: _mode = 'full', swapServiceOrder
                 </div>
                 
                 {/* Recommended company badges for mobile */}
-                <div className="md:hidden -mt-8 mr-16 ">
+                <div className="md:hidden -mt-8 mr-12 ">
                   <div className="flex flex-row justify-center -space-x-14">
                     <Image 
                       src="/recommendedcompany2021-no-bg.png" 
@@ -1123,7 +1123,7 @@ export default function FlyttoffertForm({ mode: _mode = 'full', swapServiceOrder
                       alt="Rekommenderat företag 2026" 
                       width={120} 
                       height={120} 
-                      className="w-24 h-24 object-contain relative z-60 mt-2 translate-x-2"
+                      className="w-24 h-24 object-contain relative z-[60] mt-2 translate-x-7"
                     />
                   </div>
                 </div>
